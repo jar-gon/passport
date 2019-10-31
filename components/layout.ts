3dd126390ababb3b056f4f84a815b4e8102b53eb
pangshiwei <pangshiwei@yunq.cn>
2019-10-31 15:04:43 +0800

    移除无用代码

--- components/layout.ts
+++ components/layout.ts
@@ -1,32 +1,13 @@
-import { ChangeEvent } from 'react'
-import router from 'next/router'
 import { Component } from '@billypon/react-utils/react'
-import { autobind } from '@billypon/react-decorator'
 
 import Header from './header'
 import template from './layout.pug'
 
 interface LayoutProps {
-  headerSearch: boolean
-  contentHeader: boolean
   footer: boolean
 }
 
-interface LayoutState {
-  keyword: string
-}
-
-class Layout extends Component<LayoutProps, LayoutState> {
-  @autobind()
-  search(): void {
-    router.push(`/products?keyword=${ this.state.keyword }`)
-  }
-
-  @autobind()
-  updateKeyword({ target: { value } }: ChangeEvent<HTMLInputElement>): void {
-    this.setState({ keyword: value })
-  }
-
+class Layout extends Component<LayoutProps> {
   render() {
     return template.call(this, { ...this, Header })
   }
