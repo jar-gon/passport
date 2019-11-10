475d9ecb3cb05ce8188a7fa89087f0f04de8b489
pangshiwei <pangshiwei@yunq.cn>
2019-11-10 21:52:33 +0800

    调整其他系统url的获取

--- components/header.ts
+++ components/header.ts
@@ -4,14 +4,8 @@ import { publicRuntimeConfig } from '~/utils/config'
 
 import template from './header.pug'
 
-interface HeaderState {
-  home: string
-}
-
-class Header extends Component<{ }, HeaderState> {
-  getInitialState() {
-    return { home: publicRuntimeConfig.home }
-  }
+class Header extends Component {
+  homeUrl = publicRuntimeConfig.home
 
   render() {
     return template.call(this, { ...this })
